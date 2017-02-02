12d08eaa868c8d0bb30acbcc45206f59325b139f
Andy Zhang <zch.ustc@gmail.com>
2017-02-02 11:07:59 +0800

    Remove tr1 from mac build

--- src/v8_value_hasher.h
+++ src/v8_value_hasher.h
@@ -4,13 +4,8 @@
 #include <string>
 #include <iostream>
 #include <node.h>
-#ifdef __APPLE__
-#include <tr1/unordered_set>
-#define hash std::tr1::hash
-#else
 #include <unordered_set>
 #define hash std::hash
-#endif
 
 typedef Nan::Persistent<v8::Value, Nan::CopyablePersistentTraits<v8::Value> > CopyablePersistent;
 
